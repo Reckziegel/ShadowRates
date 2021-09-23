e6339a7e7a23afeb9121f7037519ba6ab60fc485
Bernardo Reckziegel <bernardo_cse@hotmail.com>
2021-09-22 21:31:35 -0300

    adding `magrittr` as import

--- R/shadow_rates.R
+++ R/shadow_rates.R
@@ -4,35 +4,37 @@
 #'
 #' @param .data A tidy \code{tibble}.
 #' @param .group_col The maturities column.
-#' @param .dbl_col The numeric column with interest rates.
-#' @param eta A \code{double} with the threshold between high and low interest
+#' @param .dbl_col A column with interest rates.
+#' @param .eta A \code{double} with the threshold between high and low interest
 #' rates environments.
-#' @param k A \code{double} with the call strike rate. Default's to zero.
+#' @param .strike A \code{double} with the call strike rate. Default's to zero.
 #'
 #' @return A tidy \code{tibble}.
 #' @export
+#'
+#' @importFrom magrittr %>%
 #'
 #' @examples
 #' library(ggplot2)
 #'
 #' data("us_yield_curve")
 #'
-#' sr <- shadow_rates(us_yield_curve, maturity, rates)
+#' shadow <- shadow_rates(us_yield_curve, maturity, rates)
 #'
-#' ggplot(sr, aes(x = index, y = rates, color = maturity)) +
+#' ggplot(shadow, aes(x = index, y = rates, color = maturity)) +
 #'   geom_line()
-shadow_rates <- function(.data, .group_col, .dbl_col, eta = 0.015, k = 0) {
+shadow_rates <- function(.data, .group_col, .dbl_col, .eta = 0.015, .strike = 0) {
 
-  assertthat::assert_that(assertthat::is.number(eta))
-  assertthat::assert_that(assertthat::is.number(k))
+  assertthat::assert_that(assertthat::is.number(.eta))
+  assertthat::assert_that(assertthat::is.number(.strike))
 
-  .data |>
-    dplyr::group_by({{.group_col}}) |>
+  .data %>%
+    dplyr::group_by({{.group_col}}) %>%
     dplyr::mutate({{.dbl_col}} := dplyr::if_else(
-      condition = {{.dbl_col}} >= eta,
-      true      = {{.dbl_col}} + k,
-      false     = eta * (log({{.dbl_col}} / eta) + (1 / eta) * (eta + k))
+      condition = {{.dbl_col}} >= .eta,
+      true      = {{.dbl_col}} + .strike,
+      false     = .eta * (log({{.dbl_col}} / .eta) + (1 / .eta) * (.eta + .strike))
     )
-    ) |>
+    ) %>%
     dplyr::filter(!is.infinite({{.dbl_col}}))
 }
